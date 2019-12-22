2b83263249e630a1929a7b0f310a3edbbe427f11
User <ghosharpita423@gmail.com>
2019-12-22 23:16:38 +0600

    Home

--- app/src/main/java/com/example/ecommerce_demo/HomeActivity.java
+++ app/src/main/java/com/example/ecommerce_demo/HomeActivity.java
@@ -92,6 +92,7 @@ implements NavigationView.OnNavigationItemSelectedListener{
         CircleImageView profileImageView = headerView.findViewById(R.id.user_profile_image);
 
         userNameTextView.setText(Prevalent.currentOnlineUser.getName());
+        Picasso.get().load(Prevalent.currentOnlineUser.getImage()).placeholder(R.drawable.profile).into(profileImageView);
 
         recyclerView = findViewById(R.id.recycler_menu);
         recyclerView.setHasFixedSize(true);
